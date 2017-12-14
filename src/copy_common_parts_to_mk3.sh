4e3957b1cbc22bc9713f2931a1845ba7ea8b1b9a
Grégoire Saunier <gregoire.saunier@ekunn.com>
2017-12-14 18:57:38 +0100

    split mk2.5 dir from mk2_mk2s

--- src/copy_common_parts_to_mk3.sh
+++ src/copy_common_parts_to_mk3.sh
@@ -15,7 +15,25 @@
 #
 ################################################################################
 
-files="
+common_mk25_files="
+cable_clip_lcd
+cable_clip_round
+cable_clip_ziptie
+end_cap
+foot
+lcd_support_a
+lcd_support_b
+rambo_base_lower_mount
+rambo_base_upper_mount
+rod_holder
+y_idler
+y_motor_mount
+z_motor_mount_front
+z_motor_mounts
+z_tops
+"
+
+common_mk3_files="
 cable_clip_lcd
 cable_clip_round
 cable_clip_ziptie
@@ -30,11 +48,20 @@ z_motor_mounts
 z_tops
 "
 
-src_dir="../printed_parts/for_mk2_mk2s"
-dest_dir="../printed_parts/for_mk3"
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+src_dir="${SCRIPTDIR}/../printed_parts/for_mk2_mk2s"
 
-# Copy files
-for i in $files; do
+# Copy common MK2 files into MK2.5 dir
+dest_dir="${SCRIPTDIR}/../printed_parts/for_m2.5"
+for i in $common_mk25_files; do
+    echo "copy file $i"
+    cp -f "${src_dir}/stl/${i}.stl" "${dest_dir}/stl/"
+    cp -f "${src_dir}/step/${i}.step" "${dest_dir}/step/"
+done
+
+# Copy common MK2 files into MK3 dir
+dest_dir="${SCRIPTDIR}/../printed_parts/for_mk3"
+for i in $common_mk3_files; do
     echo "copy file $i"
     cp -f "${src_dir}/stl/${i}.stl" "${dest_dir}/stl/"
     cp -f "${src_dir}/step/${i}.step" "${dest_dir}/step/"
