3f358310d7107de64e989fe7a79b36a22d3b8040
Grégoire Saunier <gregoire.saunier@ekunn.com>
2017-12-21 12:23:51 +0100

    add total files counters

--- src/copy_common_parts.sh
+++ src/copy_common_parts.sh
@@ -60,7 +60,7 @@ src_dir="${SCRIPTDIR}/../printed_parts/for_mk2_mk2s"
 
 # Copy common MK2 files into MK2.5 dir
 num_files=$(echo $common_mk25_files | wc -w)
-echo -e "\nCopy to MK2.5 (${num_files} files)"
+echo -e "\nCopy ${num_files} files to MK2.5"
 dest_dir="${SCRIPTDIR}/../printed_parts/for_mk2.5"
 for i in $common_mk25_files; do
     echo "copy file $i"
@@ -68,12 +68,33 @@ for i in $common_mk25_files; do
     cp -f "${src_dir}/step/${i}.step" "${dest_dir}/step/"
 done
 
+# Count MK2.5 STL files
+num_tot_mk25_stl=$(ls ${dest_dir}/stl | wc -w)
+num_tot_mk25_step=$(ls ${dest_dir}/step | wc -w)
+
+
 # Copy common MK2 files into MK3 dir
 num_files=$(echo $common_mk3_files | wc -w)
-echo -e "\nCopy to MK3 (${num_files} files)"
+echo -e "\nCopy ${num_files} files to MK3"
 dest_dir="${SCRIPTDIR}/../printed_parts/for_mk3"
 for i in $common_mk3_files; do
     echo "copy file $i"
     cp -f "${src_dir}/stl/${i}.stl" "${dest_dir}/stl/"
     cp -f "${src_dir}/step/${i}.step" "${dest_dir}/step/"
 done
+
+# Count MK2(s) STL files
+num_tot_mk2_stl=$(ls ${src_dir}/stl | wc -w)
+num_tot_mk2_step=$(ls ${src_dir}/step | wc -w)
+
+# Count MK3 STL files
+num_tot_mk3_stl=$(ls ${dest_dir}/stl | wc -w)
+num_tot_mk3_step=$(ls ${dest_dir}/step | wc -w)
+
+# Display files counts
+echo -e "\nTotal number of MK2(s) STL files  : $num_tot_mk2_stl"
+echo      "Total number of MK2(s) STEP files : $num_tot_mk2_step"
+echo      "Total number of MK2.5 STL files   : $num_tot_mk25_stl"
+echo      "Total number of MK2.5 STEP files  : $num_tot_mk25_step"
+echo      "Total number of MK3 STL files     : $num_tot_mk3_stl"
+echo      "Total number of MK3 STEP files    : $num_tot_mk3_step"
